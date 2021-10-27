6ae22580d55917cbb2d3114764a18ff7a623aa64
cj3ton <cj3ton@gmail.com>
2021-10-27 18:36:57 +0700

    Fix

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,15 +11,15 @@ $mail->CharSet = 'utf-8';
 $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.sibnet.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'hg.rhggfh.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '3ton@sibnet.ru';                 // Наш логин
-$mail->Password = 'bhnjmkLp09';                           // Наш пароль от ящика
+$mail->Username = 'hgfh@hgfht.ru';                 // Наш логин
+$mail->Password = '9659796499';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 25;                                    // TCP port to connect to
  
-$mail->setFrom('3ton@sibnet.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('dekoder@inbox.ru');     // Add a recipient
+$mail->setFrom('fghfn@fghfh.ru', 'Pulse');   // От кого письмо 
+$mail->addAddress('fhfgh@fghfgh.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
